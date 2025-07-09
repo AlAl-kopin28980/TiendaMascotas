3d8f57f91faae9b71c58cfc8414c94b4695e6dd8
PlinStudios <plincompany2@gmail.com>
2025-07-09 16:43:24 -0400

    Se puede tener más de 2 Scene conrrectamente

--- src/main/java/Graphics/Comprar.java
+++ src/main/java/Graphics/Comprar.java
@@ -1,4 +1,25 @@
 package Graphics;
 
-public class Comprar {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public final class Comprar extends Scene {
+
+    static Comprar instance = null;
+
+    public static Comprar getInstance(){
+        if (instance!=null)
+            return instance;
+        else
+            instance = new Comprar();
+        return instance;
+    }
+    private Comprar() {
+        super(Color.cyan);
+    }
+
+    @Override
+    public void whenClick(MouseEvent e) {
+
+    }
 }
